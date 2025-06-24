ad7db9c4afff199993da8c200a5402ff19577a41
Priz <santi036@school.lu>
2025-06-24 21:07:24 +0200

    Comment update

--- assets.js
+++ assets.js
@@ -1,2 +1,3 @@
+// not necessary, follow the instructions found at the beginning of the pdf document that i made about the game
 loadSprite("kat", "bean.png");
 loadSprite("BreadToasterDefault", "BreadToasterDefault.png");
